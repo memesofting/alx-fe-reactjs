0eebe8acfe99fbe87cbd4b19fa228a232699fbb7
memesofting <bibitayoadekunle00@gmail.com>
2025-08-27 14:38:46 +0100

    created user profile cards

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,8 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Mamame" age="45" bio="Loves food" />
+      <UserProfile name="Obu Ganga" age="15" bio="Reads a lot" />
       <Footer />
     </>
   )
