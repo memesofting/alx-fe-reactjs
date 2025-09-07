7472ec1685821195f9d9d2b382df9726df02b9bd
memesofting <bibitayoadekunle00@gmail.com>
2025-09-07 01:26:21 +0100

    added usercontext to userprofile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,12 @@
+import UserContext from "./UserContext";
+import { useContext } from "react";
+
 const UserProfile = (props) => {
+
+    const userData = useContext(UserContext)
     return(
         <div className="userCard">
-            <h2>{props.name}</h2>
+            <h2>{userData.name}</h2>
             <p>Age: {props.age}</p>
             <p>Bio: {props.bio}</p>
         </div>
