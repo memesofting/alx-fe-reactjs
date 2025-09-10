6ab11630a0e69ee8deab570bc753714e59069a95
memesofting <bibitayoadekunle00@gmail.com>
2025-09-10 11:39:56 +0100

    update variables

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 const useRecipeStore = create(set => ({
     recipes: [],
     addRecipe: (newRecipe) => set(state => ({recipe: [...state.recipes, newRecipe]})),
-    setRecipe: (recipes) => set({recipes})
+    setRecipes: (recipes) => set({recipes})
 }));
 
 export default useRecipeStore;
