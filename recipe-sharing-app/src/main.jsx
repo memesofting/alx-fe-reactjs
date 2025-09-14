843423f15048c95f26aef97f6f4b3d673ca62e2e
memesofting <bibitayoadekunle00@gmail.com>
2025-09-14 23:07:39 +0100

    update router to browser router

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import router from './router.js'
 import { RouterProvider } from 'react-router'
+import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import RecipeDetails from './components/RecipeDetails.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/recipedetails" element={<RecipeDetails />} />
+      </Routes>
+  </BrowserRouter>,
 )
