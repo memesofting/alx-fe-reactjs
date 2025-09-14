1f50117f1be847978a07843b7eb7a97f48f503d2
memesofting <bibitayoadekunle00@gmail.com>
2025-09-14 23:26:02 +0100

    routes added to App component

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,21 @@
 import RecipeList from "./components/RecipeList"
 import AddRecipeForm from "./components/AddRecipeForm"
 import RecipeDetails from "./components/RecipeDetails"
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-    <AddRecipeForm />
-    <RecipeList />
-    {/* <RecipeDetails /> */}
+    <Routes>
+        <Route path="/" element={
+          <>
+          <AddRecipeForm />
+          <RecipeList />
+          </>
+      } />
+        <Route path="/recipedetails" element={<RecipeDetails />} />
+    </Routes>
     </>
   )
 }
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,17 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router'
-import { Routes, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import RecipeDetails from './components/RecipeDetails.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/recipedetails" element={<RecipeDetails />} />
-      </Routes>
+    <App />
   </BrowserRouter>,
 )
