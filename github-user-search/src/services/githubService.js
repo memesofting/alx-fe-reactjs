10e13b7c1ad92a9248d927070099d4fffe25fb95
memesofting <bibitayoadekunle00@gmail.com>
2025-09-20 00:46:08 +0100

    used base url variable

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,10 +2,10 @@ import.meta.env.VITE_APP_GITHUB_API_KEY
 
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users/{username}";
+const BASE_URL = "https://api.github.com/users/";
 
 function fetchUserData(username) {
-    return axios.get(`https://api.github.com/users/${username}`, {
+    return axios.get(`${BASE_URL}${username}`, {
         headers: {
             'content-type': 'application/json',
         }
