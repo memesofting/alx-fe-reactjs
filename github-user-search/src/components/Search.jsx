61b9624e2a790f06c321cec984402726fde1dad3
memesofting <bibitayoadekunle00@gmail.com>
2025-09-20 00:43:39 +0100

    added error message for user not found

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -5,10 +5,13 @@ function Search() {
   const [user, setUser] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [userData, setUserData] = useState(null);
+  const [error, setError] = useState("");
 
   const searchUser = (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setError("");
+
     fetchUserData(user)
       .then((response) => {
         setUserData(response.data);
@@ -16,6 +19,7 @@ function Search() {
       })
       .catch((err) => {
         console.error("Error fetching users: ", err);
+        setError("Looks like we cant find the user");
         setUserData(null);
       })
       .finally(() => {
@@ -34,9 +38,13 @@ function Search() {
         />
         <button type="submit">Search</button>
       </form>
+      
       {isLoading && (
         <p>Loading ...</p>
       )}
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
       {userData && (
         <div>
           <img src={userData.avatar_url} alt={`${userData.login}'s avatar`} />
