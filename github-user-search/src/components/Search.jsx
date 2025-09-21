e4ce573676bd1d1a666b92d6e5eb2fa6897b2887
memesofting <bibitayoadekunle00@gmail.com>
2025-09-21 18:56:46 +0100

    feat: filter users

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -3,6 +3,7 @@ import fetchUserData from "../services/githubService";
 
 function Search() {
   const [user, setUser] = useState("");
+  const [location, setLocation] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState("");
@@ -12,7 +13,19 @@ function Search() {
     setIsLoading(true);
     setError("");
 
-    fetchUserData(user)
+    const queryParts = [];
+    if (user)
+    {
+      queryParts.push(user);
+    }
+    if (location)
+    {
+      queryParts.push(`location:${location}`);
+    }
+
+    const query = queryParts.join('+');
+
+    fetchUserData(query)
       .then((response) => {
         setUserData(response.data);
         console.log(response.data);
@@ -30,19 +43,26 @@ function Search() {
     <>
       <form onSubmit={searchUser}>
         <label htmlFor="searchUser">Search github user</label>
-        <input 
-        className="bg-gray-300 text-gray-900"
+        <input
+          className="bg-gray-300 text-gray-900"
           type="text"
           value={user}
           placeholder="Enter username"
           onChange={(e) => setUser(e.target.value)}
         />
-        <button className="bg-green-700" type="submit">Search</button>
+        <label htmlFor="minRepo">Location</label>
+        <input
+          type="text"
+          value={location}
+          placeholder="Enter min repo"
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button className="bg-green-700" type="submit">
+          Search
+        </button>
       </form>
 
-      {isLoading && (
-        <p>Loading ...</p>
-      )}
+      {isLoading && <p>Loading ...</p>}
 
       {error && <p style={{ color: "red" }}>{error}</p>}
 
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,10 +2,10 @@ import.meta.env.VITE_APP_GITHUB_API_KEY
 
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users/";
+const BASE_URL = "https://api.github.com/search/users";
 
-function fetchUserData(username) {
-    return axios.get(`${BASE_URL}${username}`, {
+async function fetchUserData(query) {
+    return axios.get(`${BASE_URL}?q=${query}`, {
         headers: {
             'content-type': 'application/json',
         }
